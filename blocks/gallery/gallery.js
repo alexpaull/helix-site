9ebee78894dc2299f05b0b0610fa6cbf99571de1
Alex Paull <alexfpaull@gmail.com>
2022-10-16 16:16:53 -0400

    Gallery update
    create structure
    gallery collection css initial
    gallery select initial

--- blocks/gallery/gallery.js
+++ blocks/gallery/gallery.js
@@ -1,4 +1,14 @@
 export default function decorate(block) {
-  const cols = [...block.firstElementChild.children];
-  block.classList.add(`gallery-${cols.length}-cols`);
+    const galleryWrapper = block.parentElement;
+    const galleryPicture = document.createElement("div");
+    galleryPicture.classList.add("gallery-picture");
+    const first = block.firstElementChild;
+    block.firstElementChild.classList.add("selected");
+    galleryPicture.append(first.cloneNode(true));
+    galleryWrapper.prepend(galleryPicture);
+
+    // function for selecting new gallery image
+    const selectImage = (element) => {
+
+    }
 }
